645d18bcad2a5e32270b6fbeeceac3cea23a6e72
Efrain Perez <efrain.abperez23@gmail.com>
2025-10-12 20:09:34 -0500

    feat: added logs

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -1,7 +1,12 @@
 from json import dumps
+from logging import getLogger, INFO, Logger
+from typing import Final, Any, Dict
+
+logger: Final[Logger] = getLogger()
+logger.setLevel(INFO)
 
 
-def lambda_handler(event, context):
-    print("Received event:", event)
-    print("Received context:", context)
+def lambda_handler(event, context) -> Dict[str, Any]:
+    logger.info("Received event:", event)
+    logger.info("Received context:", context)
     return {"statusCode": 200, "body": dumps("Hello from Lambda!")}
