504f49bd2325be7938a93cceeb24ea15c828c379
Efrain Perez <efrain.abperez23@gmail.com>
2025-10-25 14:50:00 -0500

    feat: changed to default logger without colors

--- src/lambda/logger.py
+++ src/lambda/logger.py
@@ -1,49 +1,14 @@
-from typing import Final, Dict
-from logging import (
-    getLogger,
-    INFO,
-    DEBUG,
-    WARNING,
-    ERROR,
-    CRITICAL,
-    Logger,
-    StreamHandler,
-    Formatter,
-    LogRecord,
-)
+from logging import getLogger, DEBUG, StreamHandler, Formatter, Logger
+from typing import Final
 
-
-class ColorFormatter(Formatter):
-    COLORS: Final[Dict[int, str]] = {
-        DEBUG: "\033[1;94m",  # Blue
-        INFO: "\033[1;92m",  # Green
-        WARNING: "\033[1;93m",  # Yellow
-        ERROR: "\033[1;91m",  # Red
-        CRITICAL: "\033[1;95m",  # Purple
-    }
-    RESET: Final[str] = "\033[0m"
-    BOLD: Final[str] = "\033[1m"
-
-    def format(self, record: LogRecord) -> str:
-        level_color: Final[str] = self.COLORS.get(record.levelno, self.RESET)
-        record.levelname = f"{level_color}{record.levelname}{self.RESET}"
-        original_msg: Final[str] = super().format(record)
-        if record.msg:
-            formatted_msg: Final[str] = original_msg.replace(
-                record.getMessage(), f"{self.BOLD}{record.getMessage()}{self.RESET}"
-            )
-            return formatted_msg
-        return original_msg
-
-
-LOGGER: Final[Logger] = getLogger("CLEANUP_LOGGER")
+LOGGER: Final[Logger] = getLogger("report-cleanup")
 LOGGER.setLevel(DEBUG)
 LOGGER.propagate = False
 
 if not LOGGER.handlers:
     handler: Final[StreamHandler] = StreamHandler()
-    formatter: Final[ColorFormatter] = ColorFormatter(
-        "%(asctime)s - %(levelname)s - %(message)s", "%H:%M:%S"
+    formatter: Final[Formatter] = Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s", datefmt="%H:%M:%S"
     )
     handler.setFormatter(formatter)
     LOGGER.addHandler(handler)
